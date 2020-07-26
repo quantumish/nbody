76518230b48acfe0d52c13bca196ba28accf832d
David Freifeld <freifeld.david@gmail.com>
2020-07-25 17:11:27 -0700

    Ignoring problems and benchmarking

--- bench.py
+++ bench.py
@@ -1,28 +1,44 @@
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+import time
 import nbody
 
-sim = nbody.Sim(10000)
-sim.add_body(10**24, [0,150 * 10**9, 0], [1600,0,0], [0,0,0])
-sim.add_body(10**30, [0,0,0], [0,0,0], [0,0,0])
+import random
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-
-x1,x2,x3=[],[],[]
-y1,y2,y3=[],[],[]
-z1,z2,z3=[],[],[]
-for j in range(500):
-   x1.append(sim.bodies[0].position[0])
-   y1.append(sim.bodies[0].position[1])
-   z1.append(sim.bodies[0].position[2])
-   x2.append(sim.bodies[1].position[0])
-   y2.append(sim.bodies[1].position[1])
-   z2.append(sim.bodies[1].position[2])
-   sim.update()
-
-print (sim.bodies[0].net_force)
-ax.scatter3D(x1, y1, z1, label="Planet");
-ax.scatter3D(x2, y2, z2, label="Star");
-plt.legend()
+sim = nbody.Sim(1)
+times = []
+for i in range(100):
+    before = time.time()
+    for j in range(i):
+        sim.add_body(random.randrange(10**5, 10**25, 10**3), [random.randrange(-10**10, 10**10, 10**3),random.randrange(-10**10, 10**10, 10**3), random.randrange(-10**10, 10**10, 10**3)], [random.randrange(-10**4, 10**4, 10),random.randrange(-10**4, 10**4, 10),random.randrange(-10**4, 10**4, 10)], [0,0,0])
+    for j in range(1):
+        sim.update()
+    after = time.time()
+    times.append(after-before)
+    print("Loop %s completed in %s seconds" % (i, after-before))
+plt.plot(times)
 plt.show()
+# sim = nbody.Sim(1)
+# sim.add_body(10**9, [0,10**5, 0], [300,0,0], [0,0,0])
+# sim.add_body(10**15, [0,0,0], [0,0,0], [0,0,0])
+
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+
+# x1,x2,x3=[],[],[]
+# y1,y2,y3=[],[],[]
+# z1,z2,z3=[],[],[]
+# for j in range(1000):
+#    x1.append(sim.bodies[0].position[0])
+#    y1.append(sim.bodies[0].position[1])
+#    z1.append(sim.bodies[0].position[2])
+#    x2.append(sim.bodies[1].position[0])
+#    y2.append(sim.bodies[1].position[1])
+#    z2.append(sim.bodies[1].position[2])
+#    sim.update()
+
+# print (sim.bodies[0].net_force)
+# ax.scatter3D(x1, y1, z1, label="Planet");
+# ax.scatter3D(x2, y2, z2, label="Star");
+# plt.legend()
+# plt.show()
